7f627e3a9c11723c8ea4d7b6e0cba8680c7a8a44
alexscott <alexzhangshuo@gmail.com>
2019-03-03 17:02:43 +0800

    add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by AlexScott on 2019/3/3.
+//  Copyright © 2019 Alexscott. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
